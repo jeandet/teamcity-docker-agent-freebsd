cfc6c810bb166595083f260fd04c10e534dbdd41
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-26 20:26:13 +0100

    removed sudo usage and removed deadlock during image build
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,7 @@
-sudo docker build  -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -t lpp/teamcity_agent_bsd .
-sudo docker run --privileged --net=host -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --name=tcbsd -it lpp/teamcity_agent_bsd
-sudo docker stop tcbsd
-sudo docker commit tcbsd lpp/teamcity_agent_bsd
-sudo docker rm tcbsd
+docker build  -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -t lpp/teamcity_agent_bsd .
+docker run --privileged --net=host -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --name=tcbsd -it lpp/teamcity_agent_bsd &
+sleep 300
+docker stop tcbsd
+docker commit tcbsd lpp/teamcity_agent_bsd
+docker rm tcbsd
 
